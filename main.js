37a19791c0b5df0dd38044b7a1a5f0322184520f
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-05-19 11:45:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,28 +3,46 @@ Webcam.set({
     height:300,
     image_format : 'png',
     png_quality:90
-});
-
-camera = document.getElementById("camera");
+  });
 
 Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");    
 
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
 }
 
-console.log('ml5 version:', ml5.version);
+  console.log('ml5 version:', ml5.version);
+  
+  // Initialize the Image Classifier method with MobileNet
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zrtH0-ujy/',modelLoaded);
+  // When the model is loaded
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
 
 
-
-
-
-for ( index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
 }
